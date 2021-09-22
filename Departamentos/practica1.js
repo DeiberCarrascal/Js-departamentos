bee7b470b824a2ba1145516ce88f9ed4510c29b9
DeiberCarrascal <deibercarrascal@gmail.com>
2021-09-22 17:24:59 -0500

    primer commit

---
+++ Departamentos/practica1.js
@@ -0,0 +1,134 @@
+const Departamento = () => {
+    let Departamento = prompt("Ingresa un departamento");
+
+
+    switch (Departamento) {
+
+        case "Amazonas":
+            alert("Leticia");
+            break
+
+        case "Antioquia":
+            alert("Medellin");
+            break
+
+        case "Arauca":
+            alert("Arauca");
+
+        case "Atlantico":
+            alert("Barranquilla");
+            break
+
+        case "Bolivar":
+            alert("Cartajenas de Indias");
+            break
+
+        case "Boyaca":
+            alert("Tunja");
+            break
+
+        case "Caldas":
+            alert("Manizales");
+            break
+
+        case "Caqueta":
+            alert("Caqueta");
+            break
+
+        case "Casanare":
+            alert("Yopal");
+            break
+
+        case "Cauca":
+            alert("Popayan");
+            break
+
+        case "Cesar":
+            alert("Valledupar");
+            break
+
+        case "Choco":
+            alert("Quibdo");
+            break
+
+        case "Cordoba":
+            alert("Monteria");
+            break
+
+        case "Cundinamarca":
+            alert("Bogota");
+            break
+
+        case "Guainia":
+            alert("Inirida");
+            break
+
+        case "Guaviare":
+            alert("San Jose del Guaviare");
+            break
+
+        case "Huila":
+            alert("Neiva");
+            break
+
+        case "La Guajira":
+            alert("Riohacha");
+            break
+
+        case "Magdalena":
+            alert("Santa Marta");
+            break
+
+        case "Meta":
+            alert("Villavicencio");
+            break
+
+        case "Nariño":
+            alert("San Juan de Pasto");
+            break
+
+        case "Norte de Santander":
+            alert("San Jose de Cucuta");
+            break
+
+        case "Putumayo":
+            alert("Mocoa");
+            break
+
+        case "Quindio":
+            alert("Armenia");
+            break
+
+        case "Risaralda":
+            alert("Pereira");
+            break
+
+        case "San Andres y Providencia":
+            alert("San Andres");
+            break
+
+        case "Santander":
+            alert("Bucaramanga");
+            break
+
+        case "Sucre":
+            alert("Sincelejo");
+            break
+
+        case "Tolima":
+            alert("Ibague");
+            break
+
+        case "Valle del Cauca":
+            alert("Cali");
+            break
+
+        case "Vaupes":
+            alert("Mitu");
+            break
+
+        case "Vichada":
+            alert("Puerto Carreño");
+            break
+    }
+}
